698f62c997ea7690f4f1293f610dafcd35d9ac3c
Kiaya <kmanderson14@crimson.us.edu>
2020-12-10 09:19:00 -0500

    message

---
+++ PA6/frmCWID.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace PA6
+{
+    partial class frmCWID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCWID = new System.Windows.Forms.Label();
+            this.txtCWID = new System.Windows.Forms.TextBox();
+            this.btnDK = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCWID
+            // 
+            this.lblCWID.AutoSize = true;
+            this.lblCWID.Location = new System.Drawing.Point(12, 36);
+            this.lblCWID.Name = "lblCWID";
+            this.lblCWID.Size = new System.Drawing.Size(158, 24);
+            this.lblCWID.TabIndex = 0;
+            this.lblCWID.Text = "Enter Your CWID:";
+            // 
+            // txtCWID
+            // 
+            this.txtCWID.Location = new System.Drawing.Point(183, 33);
+            this.txtCWID.Name = "txtCWID";
+            this.txtCWID.Size = new System.Drawing.Size(200, 29);
+            this.txtCWID.TabIndex = 1;
+            // 
+            // btnDK
+            // 
+            this.btnDK.Location = new System.Drawing.Point(183, 89);
+            this.btnDK.Name = "btnDK";
+            this.btnDK.Size = new System.Drawing.Size(85, 27);
+            this.btnDK.TabIndex = 2;
+            this.btnDK.Text = "OK";
+            this.btnDK.UseVisualStyleBackColor = true;
+            this.btnDK.Click += new System.EventHandler(this.btnDK_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(298, 89);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(85, 27);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmCWID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 141);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDK);
+            this.Controls.Add(this.txtCWID);
+            this.Controls.Add(this.lblCWID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmCWID";
+            this.Text = "CWID";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCWID;
+        private System.Windows.Forms.TextBox txtCWID;
+        private System.Windows.Forms.Button btnDK;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
